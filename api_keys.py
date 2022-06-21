1a565913ae0fcb13aae2e9c444bafe60e3deff11
Deepa Nair <104196538+Deepsgk@users.noreply.github.com>
2022-06-21 14:38:12 +1000

    Update API key
    put the original key

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "Hi please use yours"
+weather_api_key = "88c0e4167ed06a9a751be03bc5d3721f"
 
 # Google API Key
-g_key = "please use yours"
+g_key = "AIzaSyA6ah7pK-0iF1_ar4YOBzaY1aH8akHNYZE"
